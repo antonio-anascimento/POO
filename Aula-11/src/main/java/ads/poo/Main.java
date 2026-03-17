65a35e0f7a45b9b61d401f6d97581daaa72c295e
Antonio Augusto do Nascimento <antonio.a2007@aluno.ifsc.edu.br>
2026-03-17 20:10:23 -0300

    introdução a construtor

---
+++ Aula-11/src/main/java/ads/poo/Main.java
@@ -0,0 +1,15 @@
+package ads.poo;
+
+import java.util.Scanner;
+
+
+
+
+public class Main {
+
+    static void main() {
+
+       Pessoa p = new Pessoa("Juca", "12345678900", "blabla@gmail.com");
+       IO.println(p);
+    }
+}
